82dd9f819ae62121c2515ba1bf40a2d6c61e0651
Efend1 <kujanglarang31@gmail.com>
2024-07-24 11:25:13 +0700

    upadate add session

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World ");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
